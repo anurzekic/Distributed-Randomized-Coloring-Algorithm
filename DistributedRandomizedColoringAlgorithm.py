6cbf42432506d760a3a480bd023db3350d5c6811
Anur Zekic <anur.zekic@student.tugraz.at>
2024-01-15 14:40:17 +0100

    Cleaned up

--- DistributedRandomizedColoringAlgorithm.py
+++ DistributedRandomizedColoringAlgorithm.py
@@ -8,13 +8,13 @@ def create_color_set(size):
 def select_random_candidate_color(colors):
     return random.choice(colors) 
 
-# Delta + 1 coloring
 def exchange_candidate_colors(graph):
     for node in graph.nodes:
-        graph.nodes[node]["neighbors_candidate_color"] = []
+        graph.nodes[node]["neighbors_candidate_color"] = []  # Clear previous candidate colors of neighbors 
         for neighbor_node in graph.nodes[node]["neighbors"]:            
             graph.nodes[node]["neighbors_candidate_color"].append(graph.nodes[neighbor_node]["candidate_color"])
             
+            # Move to its own function
             if graph.nodes[neighbor_node]["permanently_colored"]:
                 if graph.nodes[neighbor_node]["candidate_color"] not in graph.nodes[node]["permantently_colored_neighbors_colors"]:
                     graph.nodes[node]["permantently_colored_neighbors_colors"].append(graph.nodes[neighbor_node]["candidate_color"])
@@ -22,7 +22,8 @@ def exchange_candidate_colors(graph):
 def recolor_nodes(graph):
     for node in graph.nodes:        
         if not graph.nodes[node]["permanently_colored"]:
-            valid_colors = list(set(graph.nodes[node]["available_colors"]) - set(graph.nodes[node]["permantently_colored_neighbors_colors"]))
+            valid_colors = list(set(graph.nodes[node]["available_colors"]) - 
+                                set(graph.nodes[node]["permantently_colored_neighbors_colors"])) 
             graph.nodes[node]["candidate_color"] = select_random_candidate_color(valid_colors)
             
 def check_if_all_nodes_colored(graph):
@@ -44,19 +45,20 @@ def set_or_discard_color(graph):
         if graph.nodes[node]["candidate_color"] not in graph.nodes[node]["neighbors_candidate_color"]:
             graph.nodes[node]["permanently_colored"] = True
         else:
-            graph.nodes[node]["candidate_color"] = None            
-            # graph.nodes[node]["neighbors_candidate_color"] = []
+            graph.nodes[node]["candidate_color"] = None
+            
     print("\nAfter exchange and setting:")
     print_graph_info(graph)
 
 def initialize(graph, colors):
     for node in graph.nodes:
-        graph.nodes[node]["candidate_color"] = None
-        graph.nodes[node]["permanently_colored"] = False
-        graph.nodes[node]["available_colors"] = colors.copy()
-        graph.nodes[node]["neighbors"] = sorted(list(graph.neighbors(node)))
-        graph.nodes[node]["neighbors_candidate_color"] = []
-        graph.nodes[node]["permantently_colored_neighbors_colors"] = []
+        graph.nodes[node]["candidate_color"] = None                             # Current candidate color
+        graph.nodes[node]["permanently_colored"] = False                        # If true the candidate color is the permanent color
+        graph.nodes[node]["available_colors"] = colors.copy()                   # List of possible colors
+        graph.nodes[node]["neighbors"] = sorted(list(graph.neighbors(node)))    # Sorted (for debugging) list of neighbors
+        graph.nodes[node]["neighbors_candidate_colors"] = []                    # Candidate colors of the neighbors
+        graph.nodes[node]["permantently_colored_neighbors_colors"] = []         # Colors of permanently colored neighbors
+        
     print("Initialize:")
     print_graph_info(graph)
 
@@ -68,22 +70,20 @@ def calculate_coloring(graph):
 
 def distributed_randomized_coloring_algorithm(graph, delta):
     color_set = create_color_set(delta)
-    # Initialize
-    initialize(graph, color_set)        
+    # Initialize graph
+    initialize(graph, color_set)         
     
     while not check_if_all_nodes_colored(graph):
         recolor_nodes(graph)
         exchange_candidate_colors(graph)
         set_or_discard_color(graph)
 
-    print(f"Colored graph with {calculate_coloring(graph)} colors. Delta of graph + 1: {delta + 1}.")
-    print("Success")
-    
+    print(f"\nDelta + 1: {delta + 1}.\nColored graph with {calculate_coloring(graph)} colors.")
 
 def main():
     DELTA = 5
     # Generate a random 5-regular graph
-    G = nx.random_regular_graph(DELTA, 36)  # 10 nodes
+    G = nx.random_regular_graph(DELTA, 10)  # 10 nodes
     distributed_randomized_coloring_algorithm(G, DELTA)
     nx.draw(G, with_labels=True, font_weight='bold', node_color=[G.nodes[node]["candidate_color"] for node in G.nodes], edge_color='gray')
     plt.show()
